c6b883053025cb6130ba4bbfc2a8da2b379bfa58
IvanNugiban <87913973+IvanNugiban@users.noreply.github.com>
2024-11-13 18:34:45 +0100

    Added drawing of blocks with text and their aligment

--- main.py
+++ main.py
@@ -1,46 +1,105 @@
-import tkinter as tk
-from icalendar import Calendar, Event
+from icalendar import Calendar
 import recurring_ical_events
 import datetime
-from src.constants import TABLE_WIDTH, TABLE_HEIGHT
+from src.constants import TABLE_WIDTH, TABLE_HEIGHT, MAX_SYMBOLS
 from src.scheduler import Scheduler
 
+def index_to_color(index):
+    colors = ['#333333', '#000080', '#DC143C', '#228B22', '#4B0082', '#FF4500']
+    return colors[index % len(colors)]
+
+def time_to_row(time, rows):
+    time_mapping = {
+        '8AM': 1, '9AM': 2, '10AM': 3, '11AM': 4, '12PM': 5,
+        '1PM': 6, '2PM': 7, '3PM': 8, '4PM': 9, '5PM': 10,
+        '6PM': 11, '7PM': 12, '8PM': 13, '9PM': 14
+    }
+    time_str = time.strftime('%I%p').lstrip('0').upper()
+    time_percentage = time.minute / 60
+    return time_mapping.get(time_str, None), time_percentage
+
+def event_to_rows(event_start, event_end, rows):
+    start_row = time_to_row(event_start, rows)
+    end_row = time_to_row(event_end, rows)
+    return start_row, end_row
+
+def clamp_text(text):
+    if len(text) > MAX_SYMBOLS:
+        return text[:MAX_SYMBOLS] + "..."
+    return text
 
 def main():
 
-    # Initialize tkinter for file dialogs
-    root = tk.Tk()
-    root.withdraw()
+    columns = [
+        {"file": "C:/Users/Ivan/Downloads/Plany.ics", "name": "Ivan"},
+        {"file": "C:/Users/Ivan/Downloads/kolya.ics", "name": "Mykola"},
+        {"file": "C:/Users/Ivan/Downloads/valeria.ics", "name": "Valera"},
+    ]
 
-    # table = choose_file("ics")
-    ics = "C:/Users/Ivan/Downloads/Plany.ics"
-
-    if not ics:
+    if len(columns) == 0:
+        print("No tables selected")
         return
 
-    with open(ics, 'rb') as f:
-        ics = Calendar.from_ical(f.read())
+    if len(columns) > 6:
+        print("Too many tables selected")
+        return
 
-    today = datetime.date.today()
-    events_by_date = {}
+    for table in columns:
+        with open(table['file'], 'rb') as f:
+            table['file'] = Calendar.from_ical(f.read())
 
-    for event in recurring_ical_events.of(ics).at(today):
-        event_summary = event.get("summary")
-        event_start = event.get("dtstart").dt
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    events = {}
 
-    # Rows and columns (first row and column is empty)
-    rows = ["", '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM']
-    columns = ["", 'Ivan', 'Serhii', 'Dima', 'Drake', 'John']
+    for table in columns:
+        # Collect all events for tomorrow
+        for event in recurring_ical_events.of(table['file']).at(tomorrow):
+            event_summary = event.get("summary")
+            event_start = event.get("dtstart").dt
+            event_end = event.get("dtend").dt
 
-    scheduler = Scheduler(TABLE_WIDTH, TABLE_HEIGHT, len(rows), len(columns))
+            name = table['name']
 
+            if name not in events:
+                events[name] = []
+
+            events[name].append({
+                "start" : event_start,
+                "end" : event_end,
+                'from_to': f"{event_start.strftime('%H:%M')} - {event_end.strftime('%H:%M')}",
+                "text" : clamp_text(event_summary)
+            })
+
+    # Rows  (first row  is empty)
+    rows = ['', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00']
+    # Add 1 in columns for empty space
+    scheduler = Scheduler(TABLE_WIDTH, TABLE_HEIGHT, len(rows), len(columns) + 1)
+
+    # Draw time slots
     for row in rows:
-        scheduler.write_text(rows.index(row), 0, row)
+        scheduler.write_text( 0, rows.index(row), rows.index(row), row, font_size=14)
 
-    for column in columns:
-        scheduler.write_text(0, columns.index(column), column)
+    for index, column in enumerate(columns):
+        # Draw names, skip first column
+        scheduler.write_text( index + 1, 0, 0, column['name'], font_size=14)
+
+        for event in events[column['name']]:
+            start_row, end_row = event_to_rows(event['start'], event['end'], len(rows))
+            # Draw events blocks
+            scheduler.draw_block(index + 1, start_row[0], end_row[0], index_to_color(index), start_row[1], end_row[1])
+            # Draw text for events
+            scheduler.write_text(index + 1, start_row[0], end_row[0],
+                                 event['text'], color='white', font_size=12, font="ariblk.ttf",
+                                 align_x='left', align_y='top',
+                                 start_row_multiplier=start_row[1], end_row_multiplier=end_row[1])
+
+            scheduler.write_text(index + 1, start_row[0], end_row[0],
+                                 event['from_to'], color='white', font_size=12, font="ariblk.ttf",
+                                 align_x='left', align_y='center',
+                                 start_row_multiplier=start_row[1], end_row_multiplier=end_row[1])
 
     scheduler.show()
+    scheduler.save("output.png")
 
 if __name__ == "__main__":
     main()
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ImageDraw import ImageDraw as ImageDrawType
+from aiohttp.web_routedef import static
+
 from src.constants import FIRST_COL_WIDTH, TABLE_PADDING
 
 class Scheduler:
@@ -17,7 +19,6 @@ class Scheduler:
         self.cells = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         # Create table
         self.create()
-
 
     def create(self):
 
@@ -49,18 +50,45 @@ class Scheduler:
                 self.cells[row][col] = [x1, y1, x2, y2]
                 self.draw.rectangle([x1, y1, x2, y2], outline="black")
 
-    def write_text(self, row, col, text):
+    def write_text(self, col, start_row, end_row, text, align_x = 'center', align_y='center',
+                   font_size = 15, color = 'black', font = "arial.ttf", start_row_multiplier = 1, end_row_multiplier = 1):
 
-        if not self.cells[row][col]:
+        if not self.cells[start_row][col] or not self.cells[end_row][col]:
             return
 
-        font = ImageFont.truetype("arial.ttf", 15)
-        text_coords = self.align_text(self.cells[row][col][0], self.cells[row][col][2], self.cells[row][col][1], self.cells[row][col][3], text, font)
+        coords = self.get_centered_coords(col, start_row, end_row, start_row_multiplier, end_row_multiplier)
 
+        font = ImageFont.truetype(font, font_size)
+        text_coords = self.align_text(coords, text, font, align_x, align_y)
 
         # Get font size & adjust text position
-        self.draw.text(xy=text_coords, text=text, fill="black", font=font)
+        self.draw.text(xy=text_coords, text=text, fill=color, font=font)
 
+    def draw_block(self, col, start_row,  end_row, color, start_row_multiplier = 1, end_row_multiplier = 1):
+
+        if not self.cells[start_row][col] or not self.cells[end_row][col]:
+            return
+
+        self.draw.rectangle(self.get_centered_coords(col, start_row, end_row, start_row_multiplier, end_row_multiplier), outline="black",
+                            fill=color)
+
+    def get_cell_dimensions(self, row, col):
+        return {
+            'width' : self.cells[row][col][2] - self.cells[row][col][0],
+            'height' : self.cells[row][col][3] - self.cells[row][col][1]
+        }
+
+    def get_centered_coords(self, col, start_row, end_row, start_row_multiplier = 1, end_row_multiplier = 1):
+
+        cell_height = self.get_cell_dimensions(start_row, col)['height']
+
+        if start_row == end_row:
+            return self.cells[start_row][col]
+
+        return [self.cells[start_row][col][0] + TABLE_PADDING,
+                self.cells[start_row][col][1] + cell_height * start_row_multiplier,
+                self.cells[end_row][col][2] - TABLE_PADDING,
+                self.cells[end_row][col][3] - cell_height + cell_height * end_row_multiplier]
 
     @staticmethod
     def get_cell_center(x1, x2):
@@ -78,16 +106,50 @@ class Scheduler:
         return text_width, text_height
 
     @staticmethod
-    def align_text(x1, x2, y1, y2, text_string, font):
-        x_center = Scheduler.get_cell_center(x1, x2)
-        y_center = Scheduler.get_cell_center(y1, y2)
+    def align_text(coords, text_string, font, align_x="center", align_y="center"):
+        x_center = Scheduler.get_cell_center(coords[0], coords[2])
+        y_center = Scheduler.get_cell_center(coords[1], coords[3])
 
-        text_center = Scheduler.get_text_dimensions(text_string, font)
+        text_dimensions = Scheduler.get_text_dimensions(text_string, font)
 
-        x_center -= text_center[0] / 2
-        y_center -= text_center[1] / 2
+        if align_x == "center":
+            x = x_center - text_dimensions[0] / 2
+        elif align_x == "left":
+            x = coords[0] + TABLE_PADDING
+        elif align_x == "right":
+            x = coords[2] - text_dimensions[0] - TABLE_PADDING
 
-        return x_center, y_center
+        if align_y == "center":
+            y = y_center - text_dimensions[1] / 2
+        elif align_y == "top":
+            y = coords[1] + TABLE_PADDING
+        elif align_y == "bottom":
+            y = coords[3] - text_dimensions[1] - TABLE_PADDING
+
+        return x, y
+
+    # @staticmethod
+    # def get_font_size_for_cell(cell_coords, text):
+    #     cell_width = cell_coords[2] - cell_coords[0]
+    #     cell_height = cell_coords[3] - cell_coords[1]
+    #
+    #     # Start with a base font size
+    #     font_size = 1
+    #     font = ImageFont.truetype("arial.ttf", font_size)
+    #
+    #     # Increase font size until it exceeds cell dimensions
+    #     while True:
+    #         text_width, text_height = font.getbbox(text)[2:4]
+    #         if text_width > cell_width / 2 or text_height > cell_height / 2:
+    #             break
+    #         font_size += 1
+    #         font = ImageFont.truetype("arial.ttf", font_size)
+    #
+    #     # Return the last fitting font size
+    #     return font_size - 1
 
     def show(self):
         self.image.show()
+
+    def save(self, filepath):
+        self.image.save(filepath)
